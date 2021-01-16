ba75191e67399ac9c99df733a545423eb976fd88
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-01-16 23:37:34 +0100

    A functional counter app is completed

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ const btns = document.querySelectorAll('.btn')
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (event) => {
-        console.log(event.currentTarget)
+        const styles = event.currentTarget.classList
+        if(styles.contains('decrease')) {
+            count--
+        } else if(styles.contains('increase')) {
+            count++
+        } else {
+            count = 0
+        }
+
+        if(count > 0) {
+            value.style.color = "green"
+        }
+        if(count < 0) {
+            value.style.color = "red"
+        }
+        if(count === 0) {
+            value.style.color = "#222"
+        }
+
+        value.textContent = count
     })
 })
